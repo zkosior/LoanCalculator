91df597f9ac27134fecc9294a2b77118d2c232da
Zbigniew Kosior <zkosior@users.noreply.github.com>
2017-12-26 00:37:08 +0100

    Simple same rate scenarios.

--- ZKosior.ZopaRecruitmentTest.LoanCalculator/ThreeYearLoanQuote.cs
+++ ZKosior.ZopaRecruitmentTest.LoanCalculator/ThreeYearLoanQuote.cs
@@ -14,6 +14,16 @@
 
         public QuoteOffer CalculateFor(decimal amount)
         {
+            if (this.offers[0].Available > amount)
+            {
+                return new QuoteOffer { Amount = amount, Rate = this.offers[0].Rate };
+            }
+
+            if (this.offers[0].Available + this.offers[1].Available > amount)
+            {
+                return new QuoteOffer { Amount = amount, Rate = this.offers[0].Rate };
+            }
+
             return new QuoteOffer { Amount = 1000, Rate = 0.07m, MonthlyPayment = 30.78m, TotalPayment = 1108.10m };
         }
     }
--- ZKosior.ZopaRecruitmentTest.Tests/ThreeYearLoanQuoteTests.cs
+++ ZKosior.ZopaRecruitmentTest.Tests/ThreeYearLoanQuoteTests.cs
@@ -8,9 +8,43 @@
     public class ThreeYearLoanQuoteTests
     {
         [Test]
-        public void SuccessScenario()
+        public void RequestedAmount()
         {
-            var marketData = new List<LoanOffer>
+            List<LoanOffer> marketData = InitializeMrketData();
+
+            var quote = new ThreeYearLoanQuote(marketData).CalculateFor(1000);
+
+            Assert.AreEqual(1000m, quote.Amount);
+            Assert.AreEqual(0.07m, quote.Rate);
+            Assert.AreEqual(30.78m, quote.MonthlyPayment);
+            Assert.AreEqual(1108.10m, quote.TotalPayment);
+        }
+
+        [Test]
+        public void RequestedAmountLessThanLowestOffer_UsesSameRate()
+        {
+            var marketData = InitializeMrketData();
+
+            var quote = new ThreeYearLoanQuote(marketData).CalculateFor(400);
+
+            Assert.AreEqual(400m, quote.Amount);
+            Assert.AreEqual(0.069m, quote.Rate);
+        }
+
+        [Test]
+        public void RequestedAmountMoreThanLowestOffer_AndTheSameRate_UsesSameRate()
+        {
+            var marketData = InitializeMrketData();
+
+            var quote = new ThreeYearLoanQuote(marketData).CalculateFor(400);
+
+            Assert.AreEqual(400m, quote.Amount);
+            Assert.AreEqual(0.069m, quote.Rate);
+        }
+
+        private static List<LoanOffer> InitializeMrketData()
+        {
+            return new List<LoanOffer>
             {
                 new LoanOffer { Lender = "Bob", Rate = 0.075m, Available = 640 },
                 new LoanOffer { Lender = "Jane", Rate = 0.069m, Available = 480 },
@@ -20,13 +54,6 @@
                 new LoanOffer { Lender = "Dave", Rate = 0.074m, Available = 140 },
                 new LoanOffer { Lender = "Angela", Rate = 0.071m, Available = 60 }
             };
-
-            var quote = new ThreeYearLoanQuote(marketData).CalculateFor(1000);
-
-            Assert.AreEqual(1000m, quote.Amount);
-            Assert.AreEqual(0.07m, quote.Rate);
-            Assert.AreEqual(30.78m, quote.MonthlyPayment);
-            Assert.AreEqual(1108.10m, quote.TotalPayment);
         }
     }
 }
