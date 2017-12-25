e7c23c167782f1a2c04ee85efd0a3de20cdd508f
Zbigniew Kosior <zkosior@users.noreply.github.com>
2017-12-25 17:53:26 +0100

    Initial commit.

---
+++ Zopa.RecruitmentTest.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Zopa.RecruitmentTest
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var file = args[0];
+            var amount = decimal.Parse(args[1]);
+        }
+    }
+}
